d65e4b7a3082fe1a791df8fa0b323990c1dd8aae
Kevyn SOLA <kevyn.sola@gmail.com>
2022-10-27 15:55:02 +0200

    US #8: Créer les cartes Circuits#4 add twig file and grid cards

--- database.sql
+++ database.sql
@@ -63,16 +63,17 @@ ALTER TABLE `item`
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 
 /* TABLE OF CIRCUITS */
-CREATE TABLE `circuits` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `title` varchar(255) NOT NULL,
+CREATE TABLE `circuit` (
+  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `title` VARCHAR(255) NOT NULL,
   `size` DECIMAL(4, 1) NOT NULL,
   `content` TEXT,
-  `map` TEXT
+  `map` TEXT,
+  `trace` VARCHAR(20)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /* INSERT CIRCUITS */
-INSERT INTO `circuits` (`title`, `size`, `trace`, `content`, `map`) VALUES
-('Le loing et le canal de Briare à Montcresson', 5,5, 'Tracé Jaune', 'Le départ du circuit se situe à l\’église  de Montcresson, se garer sur le parking derrière l\’église. Les sols rencontrés sont principalement des alluvions (anciennes ou récentes) et de l\’argile à silx vers la ferme de Toisy.Le Circuit de base, 5,5 kms, tracé en jaune sur la carte, part de l\’église en direction du canal de Briare, emprunte la D117 pour traverser le canal et tourne à gauche avant le pont sur le Loing. Suivre ce chemin jusqu\’à Montambert. On longe un étang puis une zone inondable plantée de peupliers, avec la possibilité de surprendre, toute l\’année, un Grèbe castagneux, une Gallinule poule d\’eau, un Héron cendré ou même un Chevreuil. On trouve également toutes les plantes courantes vivant en milieu humide : Reine des prés, Salicaire commune, Epilobe hirsute, Grand consoude, etc … qui fleurissent en juin-juillet.', 'Carte IGN 1/25.000 ème Est, Montargis'),
+INSERT INTO `circuit` (`title`, `size`, `trace`, `content`, `map`) VALUES
+('Le loing et le canal de Briare à Montcresson', 5.5, 'Tracé Jaune', 'Le départ du circuit se situe à l\’église  de Montcresson, se garer sur le parking derrière l\’église. Les sols rencontrés sont principalement des alluvions (anciennes ou récentes) et de l\’argile à silx vers la ferme de Toisy.Le Circuit de base, 5,5 kms, tracé en jaune sur la carte, part de l\’église en direction du canal de Briare, emprunte la D117 pour traverser le canal et tourne à gauche avant le pont sur le Loing. Suivre ce chemin jusqu\’à Montambert. On longe un étang puis une zone inondable plantée de peupliers, avec la possibilité de surprendre, toute l\’année, un Grèbe castagneux, une Gallinule poule d\’eau, un Héron cendré ou même un Chevreuil. On trouve également toutes les plantes courantes vivant en milieu humide : Reine des prés, Salicaire commune, Epilobe hirsute, Grand consoude, etc … qui fleurissent en juin-juillet.', 'Carte IGN 1/25.000 ème Est, Montargis'),
 ('Canal de Briare', 7, 'Tracé Orange', 'Le circuit complémentaire, 7 km, tracé en orange, longe le canal de Briare vers le sud et contourne le Château de la Forest, il permettra d’observer des paysages typiques du plateau Gâtinais ains que la flore et la faune des zones mélangées de cultures et de bois, dans le parc du Château, de nombreux trous de pics sont visibles dans les vieux arbres.', 'Carte IGN 1/25.000 ème Est, Montargis'),
-('La vallée de la Cléryà Saint Hilaire les Andrésis et Chantecoq', 6,5, 'Tracé Jaune', 'Le départ du circuit se situe sur la route D32 (Ferrières - Courtenay), entre Chantecoq et Saint-Hilaire-les-Andrésis, au lieu-dit les Roches (2 km à l\’Est de Chantecoq), un ancien virage de la D32 servira de parking.Le Circuit de base, de 6,5 km en jaune, se situe sur un sold\’alluvions récentes, départ vers l\’est, le long de la D32, rourner à droite au premier  chemin vers la Valetterie, puis descendre jusqu\’au Marteau et longer vers l\’amont le ruisseau (sans nom, c\’est un affluent de la Cléry) jusqu\’à la passerelle ; de la Valetterie, on domine la vallée, la présence de haies et de zones cultivées permet d\’observer et d\’entendre au printemps, quand ils sont les plus actifs, une bonne vingtaine d\’espèces d\’oiseaux communs, tôt le matin, des Chevreuils se nourrissent dans la vallée.', 'Carte IGN 1/25.000 ème 2519 Ouest Château-Renard');
+('La vallée de la Cléryà Saint Hilaire les Andrésis et Chantecoq', 6.5, 'Tracé Jaune', 'Le départ du circuit se situe sur la route D32 (Ferrières - Courtenay), entre Chantecoq et Saint-Hilaire-les-Andrésis, au lieu-dit les Roches (2 km à l\’Est de Chantecoq), un ancien virage de la D32 servira de parking.Le Circuit de base, de 6,5 km en jaune, se situe sur un sold\’alluvions récentes, départ vers l\’est, le long de la D32, rourner à droite au premier  chemin vers la Valetterie, puis descendre jusqu\’au Marteau et longer vers l\’amont le ruisseau (sans nom, c\’est un affluent de la Cléry) jusqu\’à la passerelle ; de la Valetterie, on domine la vallée, la présence de haies et de zones cultivées permet d\’observer et d\’entendre au printemps, quand ils sont les plus actifs, une bonne vingtaine d\’espèces d\’oiseaux communs, tôt le matin, des Chevreuils se nourrissent dans la vallée.', 'Carte IGN 1/25.000 ème 2519 Ouest Château-Renard');
