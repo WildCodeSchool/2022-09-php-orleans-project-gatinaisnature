1d203fee2120f7934e9099c4a159a470e196e6e8
Kevyn SOLA <kevyn.sola@gmail.com>
2022-10-28 12:50:20 +0200

    add card struct to US9 #8

--- src/routes.php
+++ src/routes.php
@@ -13,5 +13,6 @@ return [
     'items/show' => ['ItemController', 'show', ['id']],
     'items/add' => ['ItemController', 'add',],
     'items/delete' => ['ItemController', 'delete',],
-    'circuits' => ['CircuitsController', 'index',]
+    'circuits' => ['CircuitsController', 'index',],
+    'circuit-page' => ['CircuitPageController', 'index'],
 ];
