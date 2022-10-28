6cc6fd545bb5062f1cb70c2ca6af9d0b81c8c5f1
Kevyn SOLA <kevyn.sola@gmail.com>
2022-10-28 10:27:09 +0200

    US #8: Créer les cartes Circuits#4 modif height min 100vh

--- src/Controller/CircuitsController.php
+++ src/Controller/CircuitsController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controller;
+
 use App\Model\CircuitManager;
 
 class CircuitsController extends AbstractController
@@ -14,6 +15,6 @@ class CircuitsController extends AbstractController
         $circuits = [];
         $circuits = $circuitManager->selectAll();
 
-        return $this->twig->render('Circuits/chooseCircuits.html.twig', ['circuits'=>$circuits]);
+        return $this->twig->render('Circuits/chooseCircuits.html.twig', ['circuits' => $circuits]);
     }
 }
