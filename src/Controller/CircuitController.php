6481e5faaa283085bd3b7c47d89809b5021cbfa3
Kevyn SOLA <kevyn.sola@gmail.com>
2022-10-28 13:09:21 +0200

    S01 us08 #51 modif size img and circuitcontroller

--- src/Controller/CircuitController.php
+++ src/Controller/CircuitController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use App\Model\CircuitManager;
 
-class CircuitsController extends AbstractController
+class CircuitController extends AbstractController
 {
     /**
      * Display circuits page
--- src/routes.php
+++ src/routes.php
@@ -13,5 +13,5 @@ return [
     'items/show' => ['ItemController', 'show', ['id']],
     'items/add' => ['ItemController', 'add',],
     'items/delete' => ['ItemController', 'delete',],
-    'circuits' => ['CircuitsController', 'index',]
+    'circuits' => ['CircuitController', 'index',]
 ];
