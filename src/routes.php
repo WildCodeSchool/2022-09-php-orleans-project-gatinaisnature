fe1ef95390af7a3f849075ebfc5e7862e05f1a93
Kevyn SOLA <kevyn.sola@gmail.com>
2022-10-29 23:32:23 +0200

    US #14: créer page form#76 add form twig + responsive form

--- src/routes.php
+++ src/routes.php
@@ -15,4 +15,5 @@ return [
     'items/show' => ['ItemController', 'show', ['id']],
     'items/add' => ['ItemController', 'add',],
     'items/delete' => ['ItemController', 'delete',],
+    'contact' => ['FormController', 'index',],
 ];
