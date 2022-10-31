41af0ef774a92d1eb9b53387801b0058b5637176
Kevyn SOLA <kevyn.sola@gmail.com>
2022-10-31 12:36:53 +0100

    US #14: Sécurisation Form#77 Secure and create table if client want to use dataUsers

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -23,36 +23,18 @@ class FormController extends AbstractController
             }
 
             if (strlen($addSub['firstname']) > self::FIRSTNAME_LENGTH) {
-                $errors[] = 'Le prénom est trop long, il doit être inférieur à ' . self::FIRSTNAME_LENGTH . 'caractères !';
+                $errors[] = 'Le prénom doit être inférieur à ' . self::FIRSTNAME_LENGTH . 'caractères !';
             }
 
             if (strlen($addSub['email']) > self::EMAIL_LENGTH) {
-                $errors[] = 'L\'adresse E-mail est trop longue, elle doit être inférieure à ' . self::EMAIL_LENGTH . 'caractères !';
+                $errors[] = 'L\'adresse E-mail doit être inférieure à ' . self::EMAIL_LENGTH . 'caractères !';
             }
 
             if (!filter_var(($addSub['email']), FILTER_VALIDATE_EMAIL)) {
                 $errors[] = 'Ceci n\'est pas un format d\'adresse Email';
             }
 
-            if (empty($addSub['lastname'])) {
-                $errors[] = 'Le nom est obligatoire';
-            }
-
-            if (empty($addSub['firstname'])) {
-                $errors[] = 'Le prénom est obligatoire';
-            }
-
-            if (empty($addSub['email'])) {
-                $errors[] = 'L\'adresse email est obligatoire';
-            }
-
-            if (empty($addSub['subject'])) {
-                $errors[] = 'Le sujet de la question est obligatoire';
-            }
-
-            if (empty($addSub['message'])) {
-                $errors[] = 'Le message est obligatoire';
-            }
+            $errors = $this->isEmpty($addSub, $errors);
 
             if (empty($errors)) {
                 header('location: /contact?answer=good');
@@ -63,5 +45,29 @@ class FormController extends AbstractController
             'Contact/form.html.twig',
             ['errors' => $errors, 'contact' => $addSub, 'answer' => $answer]
         );
+    }
+
+    public function isEmpty(array $addSub, array $errors): array
+    {
+        if (empty($addSub['lastname'])) {
+            $errors[] = 'Le nom est obligatoire';
+        }
+
+        if (empty($addSub['firstname'])) {
+            $errors[] = 'Le prénom est obligatoire';
+        }
+
+        if (empty($addSub['email'])) {
+            $errors[] = 'L\'adresse email est obligatoire';
+        }
+
+        if (empty($addSub['subject'])) {
+            $errors[] = 'Le sujet de la question est obligatoire';
+        }
+
+        if (empty($addSub['message'])) {
+            $errors[] = 'Le message est obligatoire';
+        }
+        return $errors;
     }
 }
