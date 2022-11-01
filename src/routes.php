a823f6e09c381734faf158609371446cfc8bce2c
Kevyn SOLA <kevyn.sola@gmail.com>
2022-11-01 12:30:48 +0100

    US #9: Créer Page d'UN Circuit twig#5 modif delete circuit other branch n pics too much go see in DB for content

--- src/routes.php
+++ src/routes.php
@@ -13,6 +13,5 @@ return [
     'items/show' => ['ItemController', 'show', ['id']],
     'items/add' => ['ItemController', 'add',],
     'items/delete' => ['ItemController', 'delete',],
-    'circuits' => ['CircuitsController', 'index',],
     'circuit-page' => ['CircuitsPageController', 'index'],
 ];
