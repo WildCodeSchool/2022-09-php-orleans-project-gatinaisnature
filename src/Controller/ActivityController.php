01d0fef287867fb9c548b7217f145e04ea6a7ffe
cedriclv <[cedriclevalegant@gmail.com](mailto:cedriclevalegant@gmail.com)>
2022-11-04 15:57:47 +0100

    first commit on the CRUD for activities: creation

--- src/Controller/ActivityController.php
+++ src/Controller/ActivityController.php
@@ -23,4 +23,63 @@ class ActivityController extends AbstractController
 
         return $this->twig->render('Activity/activity.html.twig', ['events' => $events, 'activities' => $activities]);
     }
+
+
+    public function getFormErrors(array $inputs, array $errors): array
+    {
+        if (!isset($inputs['title']) || empty($inputs['title'])) {
+            $errors[] = 'Le titre doit être complété';
+        }
+        if (!isset($inputs['title']) || strlen($inputs['title']) > 100) {
+            $errors[] = 'Le titre ne doit pas faire plus de 100 caracteres';
+        }
+        if (empty($inputs['description'])) {
+            $errors[] = 'La description doit être complété';
+        }
+        if (!isset($inputs['description']) || strlen($inputs['description']) > 200) {
+            $errors[] = 'La description ne doit pas faire plus de 200 caracteres';
+        }
+
+        return $errors;
+    }
+
+    public function add()
+    {
+
+        $errors = [];
+        $messages = [];
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $inputs = array_map('trim', $_POST);
+            $errors = $this->getFormErrors($inputs, $errors);
+
+            // creer le fichier image pour le mettre dans le folder upload (ce folder ne sera pas versioné)
+            $targetDir = "./assets/upload/";
+            $targetFile = $targetDir . basename($_FILES['picture']['name']);
+            $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+            //check à rajouter ensuite ici !
+            //taille/extension/mettre un unique id pour ne pas ecraser les images.
+            $allowedExtension = ['jpg','png'];
+            if (!in_array($imageFileType, $allowedExtension)) {
+                $errors[] = 'L\'image doit etre de type jpg ou png';
+            }
+            if ($_FILES['picture']['size'] > 200000) {
+                $errors[] = 'L\'image doit etre d\'une taille maximum de 200 Ko';
+            }
+
+            if (empty($errors)) {
+                // move image to upload folder
+                if (move_uploaded_file($_FILES['picture']['tmp_name'], $targetFile)) {
+                    $messages[] = 'L\'ajout est bien efféctué';
+                    $activityManager = new ActivityManager();
+                    $activityManager->insert($inputs['title'], $inputs['description'], $targetFile);
+                } else {
+                    $errors[] = 'Le fichier image n\'a pu etre ajouté';
+                }
+            } else {
+                $messages[] = 'L\'ajout n\'est pas efféctué, voici la liste des erreurs';
+            }
+        }
+        return $this->twig->render('Activity/add.html.twig', ['errors' => $errors, 'messages' => $messages]);
+    }
 }
--- src/routes.php
+++ src/routes.php
@@ -10,6 +10,7 @@ return [
 
     '' => ['HomeController', 'index',],
     'activity' => ['ActivityController', 'index',],
+    'activity/add' => ['ActivityController', 'add',],
     'items' => ['ItemController', 'index',],
     'items/edit' => ['ItemController', 'edit', ['id']],
     'items/show' => ['ItemController', 'show', ['id']],
