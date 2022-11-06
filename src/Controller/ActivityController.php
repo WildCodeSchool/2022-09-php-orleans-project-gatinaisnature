f7c75a6ef1c6801243f063e69312afc1c23c8954
cedriclv <[cedriclevalegant@gmail.com](mailto:cedriclevalegant@gmail.com)>
2022-11-06 13:04:37 +0100

    update following comments of PR

--- src/Controller/ActivityController.php
+++ src/Controller/ActivityController.php
@@ -12,6 +12,10 @@ class ActivityController extends AbstractController
      * Display home page
      */
 
+    private const MAX_LENGTH_TITLE = 100;
+    private const MAX_LENGTH_DESCRIPTION = 200;
+    private const MAX_PICTURE_SIZE = 200000;
+
     public function index(): string
     {
         $eventManager = new EventManager();
@@ -25,19 +29,19 @@ class ActivityController extends AbstractController
     }
 
 
-    public function getFormErrors(array $inputs, array $errors): array
+    public function getFormErrors(array $activity, array $errors): array
     {
-        if (!isset($inputs['title']) || empty($inputs['title'])) {
+        if (!isset($activity['title']) || empty($activity['title'])) {
             $errors[] = 'Le titre doit être complété';
         }
-        if (!isset($inputs['title']) || strlen($inputs['title']) > 100) {
-            $errors[] = 'Le titre ne doit pas faire plus de 100 caracteres';
+        if (!isset($activity['title']) || strlen($activity['title']) > self::MAX_LENGTH_TITLE) {
+            $errors[] = 'Le titre ne doit pas faire plus de ' . self::MAX_LENGTH_TITLE . ' caracteres';
         }
-        if (empty($inputs['description'])) {
+        if (empty($activity['description'])) {
             $errors[] = 'La description doit être complété';
         }
-        if (!isset($inputs['description']) || strlen($inputs['description']) > 200) {
-            $errors[] = 'La description ne doit pas faire plus de 200 caracteres';
+        if (!isset($activity['description']) || strlen($activity['description']) > self::MAX_LENGTH_DESCRIPTION) {
+            $errors[] = 'La description ne doit pas faire plus de ' . self::MAX_LENGTH_DESCRIPTION . ' caracteres';
         }
 
         return $errors;
@@ -47,40 +51,35 @@ class ActivityController extends AbstractController
     {
 
         $errors = [];
-        $messages = [];
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $inputs = array_map('trim', $_POST);
-            $errors = $this->getFormErrors($inputs, $errors);
+            $activity = array_map('trim', $_POST);
+            $errors = $this->getFormErrors($activity, $errors);
 
             // creer le fichier image pour le mettre dans le folder upload (ce folder ne sera pas versioné)
             $targetDir = "./assets/upload/";
             $imageFileType = strtolower(pathinfo($_FILES['picture']['name'], PATHINFO_EXTENSION));
             $imageFileName = pathinfo($_FILES['picture']['name'])['filename'];
             $targetFile = $targetDir . uniqid($imageFileName) . '.' . $imageFileType;
-            //check à rajouter ensuite ici !
-            //taille/extension/mettre un unique id pour ne pas ecraser les images.
             $allowedExtension = ['jpg','png'];
             if (!in_array($imageFileType, $allowedExtension)) {
-                $errors[] = 'L\'image doit etre de type jpg ou png';
+                $errors[] = 'L\'image doit etre de type ' . implode(", ", $allowedExtension);
             }
-            if ($_FILES['picture']['size'] > 200000) {
-                $errors[] = 'L\'image doit etre d\'une taille maximum de 200 Ko';
+            if ($_FILES['picture']['size'] > self::MAX_PICTURE_SIZE) {
+                $errors[] = 'L\'image doit etre avoir une taille maximum de ' . self::MAX_PICTURE_SIZE / 1000 . ' Ko';
             }
 
             if (empty($errors)) {
                 // move image to upload folder
                 if (move_uploaded_file($_FILES['picture']['tmp_name'], $targetFile)) {
-                    $messages[] = 'L\'ajout est bien efféctué';
                     $activityManager = new ActivityManager();
-                    $activityManager->insert($inputs['title'], $inputs['description'], $targetFile);
+                    $activityManager->insert($activity['title'], $activity['description'], $targetFile);
+                    header('Location: /activity');
                 } else {
                     $errors[] = 'Le fichier image n\'a pu etre ajouté';
                 }
-            } else {
-                $messages[] = 'L\'ajout n\'est pas efféctué, voici la liste des erreurs';
             }
         }
-        return $this->twig->render('Activity/add.html.twig', ['errors' => $errors, 'messages' => $messages]);
+        return $this->twig->render('Activity/add.html.twig', ['errors' => $errors]);
     }
 }
