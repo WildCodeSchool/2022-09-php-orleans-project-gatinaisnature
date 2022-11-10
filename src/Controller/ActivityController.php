b3fe28b727b8d841efb6aa387efc4b0ee58c56c1
cedriclv <[cedriclevalegant@gmail.com](mailto:cedriclevalegant@gmail.com)>
2022-11-10 10:51:28 +0100

    first commit for this US/ update fonction without show function

--- src/Controller/ActivityController.php
+++ src/Controller/ActivityController.php
@@ -90,4 +90,49 @@ class ActivityController extends AbstractController
         }
         return $this->twig->render('Activity/add.html.twig', ['errors' => $errors]);
     }
+
+        /**
+     * Edit a specific activity
+     */
+    public function edit(int $id)
+    {
+        $errors = [];
+        $activityManager = new ActivityManager();
+        $activity = $activityManager->selectOneById($id);
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $activity = array_map('trim', $_POST);
+            $errors = $this->getFormErrors($activity, $errors);
+
+            // create the image file to put it in the upload folder (without versioning)
+            $targetFile = '';
+            if ($_FILES['picture']['name'] > 0) {
+                $targetDir = "./assets/upload/";
+                $imageFileType = strtolower(pathinfo($_FILES['picture']['name'], PATHINFO_EXTENSION));
+                $imageFileName = pathinfo($_FILES['picture']['name'])['filename'];
+                $targetFile = $targetDir . uniqid($imageFileName) . '.' . $imageFileType;
+                $allowedExtension = ['jpg','png'];
+                if (!in_array($imageFileType, $allowedExtension)) {
+                    $errors[] = 'L\'image doit être de type ' . implode(", ", $allowedExtension);
+                }
+                if ($_FILES['picture']['size'] > self::MAX_PICTURE_SIZE) {
+                    $errors[] = 'L\'image doit avoir une taille maxi de ' . self::MAX_PICTURE_SIZE / 1000 . ' Ko';
+                }
+            }
+            if (empty($errors)) {
+                // move image to upload folder
+                if ($_FILES['picture']['name'] > 0) {
+                    if (move_uploaded_file($_FILES['picture']['tmp_name'], $targetFile)) {
+                        $activityManager->update($activity, $targetFile);
+                        header('Location: /activity');
+                    } else {
+                        $errors[] = 'Le fichier image n\'a pu être ajouté';
+                    }
+                } else {
+                    $activityManager->update($activity, null);
+                    header('Location: /activity');
+                }
+            }
+        }
+        return $this->twig->render('Activity/edit.html.twig', ['activity' => $activity, 'errors' => $errors]);
+    }
 }
