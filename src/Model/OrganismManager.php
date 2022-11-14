377ca9c4dfa756e2e3272418514d0f6466e9f1de
Kevyn SOLA <kevyn.sola@gmail.com>
2022-11-14 20:34:02 +0100

    add add method of organism admin

--- src/Model/OrganismManager.php
+++ src/Model/OrganismManager.php
@@ -8,12 +8,16 @@ class OrganismManager extends AbstractManager
 {
     public const TABLE = 'organism';
 
-    public function insert($name, $link, $picture)
+    public function insert(string $name, string $link, string $picture): void
     {
-        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`name`, `link`, `picture`) VALUES (:name, :link, :picture)");
-        $statement->bindValue('name', $name, PDO::PARAM_STR);
-        $statement->bindValue('link', $link, PDO::PARAM_STR);
-        $statement->bindValue('picture', $picture, PDO::PARAM_STR);
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (
+            `name`,
+            `link`,
+            `picture`
+        ) VALUES (:name, :link, :picture)");
+        $statement->bindValue(':name', $name, PDO::PARAM_STR);
+        $statement->bindValue(':link', $link, PDO::PARAM_STR);
+        $statement->bindValue(':picture', $picture, PDO::PARAM_STR);
 
         $statement->execute();
     }
