300b6ee9e0d4276f34e0df29ad65556e21d8775f
cedriclv <[cedriclevalegant@gmail.com](mailto:cedriclevalegant@gmail.com)>
2022-11-16 09:54:42 +0100

    point exclamation, balise button et deux points derriere les intitulés

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -14,16 +14,16 @@ class EventController extends AbstractController
     public function getFormErrors(array $activity, array $errors): array
     {
         if (!isset($activity['title']) || empty($activity['title'])) {
-            $errors[] = 'Le titre doit être complété';
+            $errors[] = 'Le titre doit être complété !';
         }
         if (!isset($activity['title']) || strlen($activity['title']) > self::MAX_LENGTH_TITLE) {
-            $errors[] = 'Le titre ne doit pas faire plus de ' . self::MAX_LENGTH_TITLE . ' caractères';
+            $errors[] = 'Le titre ne doit pas faire plus de ' . self::MAX_LENGTH_TITLE . ' caractères !';
         }
         if (empty($activity['description'])) {
-            $errors[] = 'La description doit être complétée';
+            $errors[] = 'La description doit être complétée !';
         }
         if (!isset($activity['description']) || strlen($activity['description']) > self::MAX_LENGTH_DESCRIPTION) {
-            $errors[] = 'La description ne doit pas faire plus de ' . self::MAX_LENGTH_DESCRIPTION . ' caractères';
+            $errors[] = 'La description ne doit pas faire plus de ' . self::MAX_LENGTH_DESCRIPTION . ' caractères !';
         }
 
         return $errors;
@@ -45,10 +45,10 @@ class EventController extends AbstractController
             $targetFile = $targetDir . uniqid($imageFileName) . '.' . $imageFileType;
             $allowedExtension = ['jpg','png','webp'];
             if (!in_array($imageFileType, $allowedExtension)) {
-                $errors[] = 'L\'image doit être de type ' . implode(", ", $allowedExtension);
+                $errors[] = 'L\'image doit être de type ' . implode(", ", $allowedExtension) . ' !';
             }
             if ($_FILES['picture']['size'] > self::MAX_PICTURE_SIZE) {
-                $errors[] = 'L\'image doit avoir une taille maximum de ' . self::MAX_PICTURE_SIZE / 1000 . ' Ko';
+                $errors[] = 'L\'image doit avoir une taille maximum de ' . self::MAX_PICTURE_SIZE / 1000 . ' Ko !';
             }
 
             if (empty($errors)) {
@@ -58,7 +58,7 @@ class EventController extends AbstractController
                     $eventManager->insert($event, $targetFile);
                     header('Location: /event/indexAdmin');
                 } else {
-                    $errors[] = 'Le fichier image n\'a pu être ajouté';
+                    $errors[] = 'Le fichier image n\'a pu être ajouté !';
                 }
             }
         }
